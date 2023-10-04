0a3c8ab5b370251ecd46bc54b16c4de9f5865294
Phan Nguyễn Hữu Phước <138502336+phacko11@users.noreply.github.com>
2023-10-04 17:15:52 +0700

    Create polynomocail.cpp

---
+++ polynomocail.cpp
@@ -0,0 +1,33 @@
+void Polynomial::insertTerm(const Term& term) {
+    // STUDENT ANSWER
+    if (term.coeff == 0) return;
+    if (this->terms->size() == 0) {
+        this->terms->add(term);
+        return;
+    }
+    int i = 0;
+    for (SLinkedList<Term>::Iterator it = this->terms->begin(); it != this->terms->end(); ++it){
+        if (term.exp == (*it).exp){
+            (*it).coeff += term.coeff;
+            if ((*it).coeff == 0) it.remove();
+            break;
+        }
+        else if (term.exp > (*it).exp){
+            this->terms->add(i, term);
+            break;
+        }
+        else if (i == this->terms->size() - 1) {
+            this->terms->add(term);
+            break;
+        }
+        ++i;
+    }
+    
+}
+
+void Polynomial::insertTerm(double coeff, int exp) {
+    // STUDENT ANSWER
+    Term term(coeff, exp);
+    insertTerm(term);
+    
+}
