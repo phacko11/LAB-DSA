5d7578c6822b4418d66f80fef0fbf8e0bee56848
Phan Nguyễn Hữu Phước <138502336+phacko11@users.noreply.github.com>
2023-12-04 08:52:36 +0700

    Create findPair.cpp

---
+++ Search/findPair.cpp
@@ -0,0 +1,25 @@
+bool findPairs(int arr[], int n, pair<int,int>& pair1, pair<int, int>& pair2)
+{
+   // TODO: If there are two pairs satisfy the condition, assign their values to pair1, pair2 and return true. Otherwise, return false.
+   map<int, pair<int, int>> Hash;
+
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = i + 1; j < n; ++j)
+		{
+			int sum = arr[i] + arr[j];
+			if (Hash.find(sum) == Hash.end())
+				Hash[sum] = make_pair(i, j);
+
+			else 
+			{
+				pair<int,int>temp = Hash[sum];
+				pair1.first = arr[temp.first];
+				pair1.second = arr[temp.second];
+				pair2 = make_pair(arr[i],arr[j]);
+				return true;
+			}
+		}
+	}
+	return false;
+}
